c573b1279609cf7e7eae8d86a6e5daa7576a1784
heexji <nitej394@naver.com>
2025-07-15 23:13:05 +0900

    refactor: 내 프로필 관련 api 리팩토링

--- src/main/java/com/umc/banddy/domain/mypage/profile/web/controller/MyProfileMediaController.java
+++ src/main/java/com/umc/banddy/domain/mypage/profile/web/controller/MyProfileMediaController.java
@@ -2,6 +2,7 @@ package com.umc.banddy.domain.mypage.profile.web.controller;
 
 import com.umc.banddy.domain.mypage.profile.service.MyProfileMediaService;
 import com.umc.banddy.domain.mypage.profile.web.dto.ProfileMediaUploadResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,9 @@ public class MyProfileMediaController {
     @PostMapping("/media")
     public ResponseEntity<ProfileMediaUploadResponse> uploadProfileMedia(
             @RequestPart("file") MultipartFile file,
-            @RequestHeader("memberId") Long memberId
+            HttpServletRequest request
     ) {
-        String url = myProfileMediaService.uploadProfileMedia(file, memberId);
+        String url = myProfileMediaService.uploadProfileMedia(file, request);
         return ResponseEntity.ok(new ProfileMediaUploadResponse(url));
     }
 }
--- src/main/java/com/umc/banddy/domain/mypage/profile/web/dto/MyProfileUpdateRequest.java
+++ src/main/java/com/umc/banddy/domain/mypage/profile/web/dto/MyProfileUpdateRequest.java
@@ -1,17 +1,37 @@
 package com.umc.banddy.domain.mypage.profile.web.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
+
+import java.util.List;
 
 @Getter
-@Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class MyProfileUpdateRequest {
+
     private String nickname;
     private Integer age;
-    private String gender;
+    private String gender; // "FEMALE", "MALE"
+
     private String region;
     private String district;
+
     private String bio;
+
+    private AvailableSessions availableSessions;
+
+    private List<String> preferredParts;
+    private List<String> genres;
+    private List<String> artists;
+    private List<String> keywords;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AvailableSessions {
+        private String vocal;     // BEGINNER, INTERMEDIATE, ADVANCED
+        private String instrument;
+    }
 }
+
