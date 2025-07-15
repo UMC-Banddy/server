e0ca8316777ce866c36b59a8181b6ae64665c2ba
heexji <nitej394@naver.com>
2025-07-15 22:23:56 +0900

    fix: sns 엔티티 수정

--- src/main/java/com/umc/banddy/domain/member/domain/Genre.java
+++ src/main/java/com/umc/banddy/domain/member/domain/Genre.java
@@ -1,5 +1,6 @@
 package com.umc.banddy.domain.member.domain;
 
+import com.umc.banddy.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -8,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class Genre {
+public class Genre extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
