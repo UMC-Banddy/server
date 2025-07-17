76d6237a43d43c5aa85b0136e5b6fea4d558039b
heexji <nitej394@naver.com>
2025-07-17 19:00:25 +0900

    feat: 비슷한 유저가 많이 저장한 곡/아티스트 조회 기능 구현

---
+++ src/main/java/com/umc/banddy/domain/mypage/similarartist/service/SimilarArtistService.java
@@ -0,0 +1,38 @@
+package com.umc.banddy.domain.mypage.similarartist.service;
+
+import com.umc.banddy.domain.member.domain.Member;
+import com.umc.banddy.domain.member.repository.MemberRepository;
+import com.umc.banddy.domain.music.artist.domain.Artist;
+import com.umc.banddy.domain.music.artist.repository.MemberArtistRepository;
+import com.umc.banddy.domain.mypage.similarartist.converter.SimilarArtistConverter;
+import com.umc.banddy.domain.mypage.similarartist.web.dto.SimilarArtistResponse;
+import com.umc.banddy.global.util.MemberSimilarityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SimilarArtistService {
+
+    private final MemberRepository memberRepository;
+    private final MemberArtistRepository memberArtistRepository;
+    private final MemberSimilarityUtil similarityUtil;
+
+    public List<SimilarArtistResponse> getArtistsSavedBySimilarUsers(Long loginMemberId) {
+        Member loginMember = memberRepository.findById(loginMemberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        // 유사한 유저들 추출
+        List<Member> similarMembers = similarityUtil.findSimilarMembers(loginMember);
+
+        // 유사한 유저들이 저장한 아티스트 인기순 정렬 후 상위 5개 추출
+        List<Artist> artists = memberArtistRepository.findTopSavedArtistsByMembers(similarMembers, 5);
+
+        return artists.stream()
+                .map(SimilarArtistConverter::toResponse)
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/com/umc/banddy/domain/other/profile/repository/MemberTagRepository.java
+++ src/main/java/com/umc/banddy/domain/other/profile/repository/MemberTagRepository.java
@@ -1,9 +1,19 @@
 package com.umc.banddy.domain.other.profile.repository;
 
+import com.umc.banddy.domain.member.domain.Member;
 import com.umc.banddy.domain.other.profile.domain.mapping.MemberTag;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberTagRepository extends JpaRepository<MemberTag, Long> {
+    List<MemberTag> findByMember(Member member);
+
     List<MemberTag> findByMemberId(Long memberId);
+
+    // 현재 회원을 제외한, 태그가 존재하는 모든 회원 조회 (유사도 비교용)
+    @Query("SELECT DISTINCT mt.member FROM MemberTag mt WHERE mt.member.id <> :excludeId")
+    List<Member> findAllMembersExcept(@Param("excludeId") Long excludeId);
 }
