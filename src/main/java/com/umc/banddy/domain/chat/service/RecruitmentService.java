74fe407da66cf1ab0e4837c8bcf047b4a2f0ffc5
J2H <jh.jeon3233@gmail.com>
2025-07-20 18:21:48 +0900

    feat:밴드 모집 생성 및 수정 구현

--- src/main/java/com/umc/banddy/domain/chat/service/RecruitmentService.java
+++
@@ -1,9 +0,0 @@
-//package com.umc.banddy.domain.chat.service;
-//
-//import com.umc.banddy.domain.chat.web.dto.RecruitmentRequest;
-//import com.umc.banddy.domain.chat.web.dto.RecruitmentResponse;
-//
-//public interface RecruitmentService {
-//
-//    public RecruitmentResponse createBand(RecruitmentRequest request);
-//}
