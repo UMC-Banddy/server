c837ef2dc5e96f7f5b7fdb7988fd9bf58f54e977
yeondub1121 <kssjshyjj@naver.com>
2025-07-25 22:55:26 +0900

    refactor: 토큰 검증 리팩토링

--- src/main/java/com/umc/banddy/global/security/jwt/JwtRequestFilter.java
+++ src/main/java/com/umc/banddy/global/security/jwt/JwtRequestFilter.java
@@ -1,5 +1,7 @@
 package com.umc.banddy.global.security.jwt;
 
+import com.umc.banddy.global.apiPayload.code.status.ErrorStatus;
+import com.umc.banddy.global.apiPayload.exception.handler.AuthHandler;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,21 +29,32 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         String authHeader = request.getHeader("Authorization");
 
+        // Authorization 헤더가 존재하고 Bearer로 시작하는 경우
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
 
-            if (jwtTokenUtil.validateToken(token)) {
-                String email = jwtTokenUtil.getEmailFromToken(token);
+            try {
+                //  토큰 유효성 검사 (유효하지 않으면 예외 발생)
+                if (jwtTokenUtil.validateToken(token)) {
+                    String email = jwtTokenUtil.getEmailFromToken(token);
 
-                User principal = new User(email, "", Collections.emptyList());
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        principal, null, principal.getAuthorities());
+                    // 인증 객체 등록
+                    User principal = new User(email, "", Collections.emptyList());
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
 
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    throw new AuthHandler(ErrorStatus.INVALID_TOKEN); // 유효하지 않으면 에러 던짐
+                }
+
+            } catch (Exception e) {
+                throw new AuthHandler(ErrorStatus.INVALID_TOKEN); // 파싱 중 예외도 동일하게 처리
             }
         }
 
+        // 4. 토큰이 없거나 정상 인증된 경우 다음 필터 진행
         chain.doFilter(request, response);
     }
 }
