e0b3615d01f11591dfc31398ffeaf12d61305f09
J2H <jh.jeon3233@gmail.com>
2025-07-28 11:06:02 +0900

    wip

--- src/main/java/com/umc/banddy/global/config/SecurityConfig.java
+++ src/main/java/com/umc/banddy/global/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -59,19 +60,17 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://localhost:4040"      // 테스트 용
         ));
-        wsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
-        wsConfig.setAllowedHeaders(Arrays.asList("*"));
+        wsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "X-Requested-With"));
         wsConfig.setAllowCredentials(true);   // WS에는 자격증명 허용
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000","http://localhost:4040"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/ws/**", wsConfig);
-        source.registerCorsConfiguration("/ws-raw/**", wsConfig);
         source.registerCorsConfiguration("/**", config);
 
         return source;
